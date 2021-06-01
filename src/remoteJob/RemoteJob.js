a3248b310a86197366f2a276c081c8e0ed45623b
Alexey Basalov <6y6jlb@gmail.com>
2021-06-01 22:32:12 +0300

    some effect/ difficult text and position in main

--- src/remoteJob/RemoteJob.js
+++ src/remoteJob/RemoteJob.js
@@ -3,18 +3,17 @@ import containerStyle from '../common/styles/Container.module.scss'
 import Title from "../common/components/title/Title";
 import Tada from 'react-reveal/Tada';
 
-const RemoteJob = () =>{
+const RemoteJob = () => {
     return (
 
         <div className={style.remoteJobBlock}>
             <div className={`${containerStyle.mainContainer} ${style.container}`}>
-            <Tada>
-                <Title even={false} title={'remote job'}/>
-            <Tada>
-                <div className={style.insideWrapper}>
-                    <p className={style.description}> yes </p>
-                </div>
-            </Tada>
+                    <Title even={false} title={'remote job'}/>
+                <Tada>
+                    <div className={style.insideWrapper}>
+                        <p className={style.description}> yes </p>
+                    </div>
+                </Tada>
             </div>
         </div>
     )
