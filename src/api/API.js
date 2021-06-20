1aa5a589afa7bbf073c8f43d7c610b986de99375
Alexey Basalov <6y6jlb@gmail.com>
2021-06-20 17:12:37 +0300

    test from pages

--- src/api/API.js
+++ src/api/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const API = {
     test: (contacts,name,message)=>{
-        return axios.post('http://localhost:3010/sendMessage',{contacts,name,message})
+        return axios.post('https://smtp-nodejs-portfolio-6y6jlb.herokuapp.com//sendMessage',{contacts,name,message})
 
     }
 }
